2bb75987ca2af25ee0cc19f41a6eecca0cf667c0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-11-29 18:00:09 +0000

    Consolidate create and edit routes, with the added benefit of autosave

--- pages/stops/[_id]/edit.js
+++ pages/stops/[_id]/edit.js
@@ -1,18 +1,17 @@
 import { useRouter } from 'next/router';
-import Button from '../../../components/Button';
 import PageContainer from '../../../components/PageContainer';
-import Toolbar from '../../../components/Toolbar';
-import Group from '../../../components/Group';
+import Pannel from '../../../components/Pannel';
 import { Grid, GridCell } from '../../../components/Grid';
-import { IoSave, IoClose } from 'react-icons/io5';
+import { CheckboxCard } from '../../../components/CheckboxCard';
 import { useForm, yupResolver } from '@mantine/form';
-import { TextInput, LoadingOverlay, Switch } from '@mantine/core';
+import { TextInput, Switch, Button, Group, Alert, Flex, Text, NumberInput, Textarea } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
 import Schema from '../../../schemas/Stop';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import API from '../../../services/API';
 import notify from '../../../services/notify';
 import useSWR from 'swr';
+import { TbArrowLeft, TbRotate, TbShieldCheck, TbAlertCircle } from 'react-icons/tb';
 
 export default function StopsEdit() {
   //
@@ -20,112 +19,181 @@ export default function StopsEdit() {
   const router = useRouter();
   const { _id } = router.query;
 
-  const { data: customer, mutate } = useSWR(`/api/customers/${_id}`);
+  const { data: stop, mutate } = useSWR(_id && `/api/stops/${_id}`);
 
   const hasUpdatedFields = useRef(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState();
 
   const form = useForm({
-    schema: yupResolver(Schema),
+    validateInputOnBlur: true,
+    validateInputOnChange: true,
+    clearInputErrorOnChange: true,
+    validate: yupResolver(Schema),
     initialValues: {
-      first_name: '',
-      last_name: '',
-      tax_region: '',
-      tax_number: '',
-      contact_email: '',
-      send_invoices: false,
-      reference: '',
-      birthday: '',
+      unique_code: '',
+      name: '',
+      short_name: '',
+      description: '',
+      latitude: 0,
+      longitude: 0,
+      features: {
+        has_bench: false,
+        has_crossing: false,
+        has_flag: false,
+        has_abusive_parking: false,
+      },
     },
   });
 
   useEffect(() => {
-    if (!hasUpdatedFields.current && customer) {
+    if (!hasUpdatedFields.current && stop) {
       form.setValues({
-        first_name: customer.first_name || '',
-        last_name: customer.last_name || '',
-        tax_region: customer.tax_region || '',
-        tax_number: customer.tax_number || '',
-        contact_email: customer.contact_email || '',
-        send_invoices: customer.send_invoices,
-        reference: customer.reference || '',
-        birthday: customer.birthday ? new Date(customer.birthday) : '',
+        unique_code: stop.unique_code || '',
+        name: stop.name || '',
+        short_name: stop.short_name || '',
+        description: stop.description || '',
+        latitude: stop.latitude || 0,
+        longitude: stop.longitude || 0,
+        features: {
+          has_bench: stop.has_bench || false,
+          has_crossing: stop.has_crossing || false,
+          has_flag: stop.has_flag || false,
+          has_abusive_parking: stop.has_abusive_parking || false,
+        },
       });
+      form.resetDirty();
       hasUpdatedFields.current = true;
     }
-  }, [customer, form]);
+  }, [stop, form]);
 
-  function handleCancel() {
-    router.push(`/customers/${_id}`);
-  }
-
-  async function handleSave(values) {
-    try {
-      setIsLoading(true);
-      notify('new', 'loading', 'Saving changes...');
-      await API({ service: 'customers', resourceId: _id, operation: 'edit', method: 'PUT', body: values });
-      mutate({ ...customer, ...values });
-      router.push(`/customers/${_id}`);
-      notify('new', 'success', 'Changes saved!');
-    } catch (err) {
-      console.log(err);
-      setIsLoading(false);
-      notify('new', 'error', 'An error occurred.');
+  const handleClose = async () => {
+    if (form.isValid()) {
+      await handleSave(form.values);
+      router.push(`/stops/${_id}`);
     }
-  }
+  };
+
+  const handleSave = useCallback(
+    async (values) => {
+      try {
+        setIsLoading(true);
+        notify('new', 'loading', 'Saving changes...');
+        await API({ service: 'stops', resourceId: _id, operation: 'edit', method: 'PUT', body: values });
+        mutate({ ...stop, ...values });
+        notify('new', 'success', 'Changes saved!');
+        setIsLoading(false);
+        setIsError(false);
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(err);
+        notify('new', 'error', err.message || 'An error occurred.');
+      }
+    },
+    [_id, stop, mutate]
+  );
+
+  useEffect(() => {
+    const autoSaveInterval = setInterval(async () => {
+      if (form.isDirty() && form.isValid()) {
+        await handleSave(form.values);
+        hasUpdatedFields.current = false;
+      }
+    }, 1000);
+    return () => clearInterval(autoSaveInterval);
+  }, [form, handleSave]);
 
   return (
     <form onSubmit={form.onSubmit(handleSave)}>
-      <PageContainer
-        title={
-          'Customers › ' +
-          (form.values.first_name || form.values.last_name
-            ? `${form.values.first_name} ${form.values.last_name}`
-            : 'New Customer')
-        }
-      >
-        <LoadingOverlay visible={isLoading} />
-        <Toolbar>
-          <Button type={'submit'} icon={<IoSave />} label={'Save'} color={'primary'} />
-          <Button icon={<IoClose />} label={'Cancel'} onClick={handleCancel} />
-        </Toolbar>
+      <PageContainer title={`Stops › ${form.values.unique_code}`}>
+        {isError ? (
+          <Alert icon={<TbAlertCircle />} title={`Error: ${isError.message}`} color='red'>
+            <Flex gap='md' align='flex-start' direction='column'>
+              <Text>{isError.description || 'No solutions found for this error.'}</Text>
+              <Button
+                type={'submit'}
+                variant='default'
+                color='red'
+                leftIcon={<TbRotate />}
+                disabled={!form.isValid()}
+                loading={isLoading}
+              >
+                Try saving again
+              </Button>
+            </Flex>
+          </Alert>
+        ) : (
+          <Group>
+            <Button leftIcon={<TbArrowLeft />} onClick={handleClose} disabled={!form.isValid()}>
+              Save & Close
+            </Button>
+            <Button
+              type={'submit'}
+              leftIcon={<TbShieldCheck />}
+              variant='light'
+              color='green'
+              loading={isLoading}
+              disabled={!form.isValid()}
+            >
+              {isLoading ? 'Saving changes...' : 'Changes are saved'}
+            </Button>
+          </Group>
+        )}
 
-        <Group title={'Customer Details'}>
+        <Pannel title={'General Details'}>
           <Grid>
-            <TextInput label={'First Name'} placeholder={'Alberta'} {...form.getInputProps('first_name')} />
-            <TextInput label={'Last Name'} placeholder={'Soares'} {...form.getInputProps('last_name')} />
-            <DatePicker label={'Birthday'} placeholder={'Pick a date'} {...form.getInputProps('birthday')} />
-            <TextInput label={'Reference'} placeholder={'PT'} {...form.getInputProps('reference')} />
-          </Grid>
-        </Group>
-
-        <Group title={'Invoicing'}>
-          <Grid>
-            <GridCell>
-              <Switch
-                label='Send Invoices'
-                checked={form.values.send_invoices}
-                onChange={({ currentTarget }) => form.setFieldValue('send_invoices', currentTarget.checked)}
-              />
-            </GridCell>
+            <TextInput label={'Stop Code'} placeholder={'000000'} {...form.getInputProps('unique_code')} />
+            <div />
           </Grid>
           <Grid>
-            <TextInput label={'Tax Region'} placeholder={'PT'} maxLength={2} {...form.getInputProps('tax_region')} />
+            <TextInput label={'Name'} placeholder={'Avenida da República (ICNF)'} {...form.getInputProps('name')} />
             <TextInput
-              label={'Tax Number'}
-              placeholder={'500 100 200'}
-              maxLength={11}
-              {...form.getInputProps('tax_number')}
+              label={'Short Name'}
+              placeholder={'Av. República (ICNF)'}
+              {...form.getInputProps('short_name')}
             />
           </Grid>
           <Grid>
-            <TextInput
-              label={'Contact Email'}
-              placeholder={'email@icloud.com'}
-              {...form.getInputProps('contact_email')}
+            <Textarea label={'Description'} autosize minRows={2} {...form.getInputProps('description')} />
+          </Grid>
+        </Pannel>
+
+        <Pannel title={'Location'}>
+          <Grid>
+            <NumberInput label={'Latitude'} defaultValue={0} precision={5} {...form.getInputProps('latitude')} />
+            <NumberInput label={'Longitude'} defaultValue={0} precision={5} {...form.getInputProps('longitude')} />
+          </Grid>
+        </Pannel>
+
+        <Pannel title={'Features'}>
+          <Grid>
+            <CheckboxCard
+              title={'Has Bench?'}
+              description={'If this stop has a bench'}
+              checked={form.values.features.has_bench}
+              onChange={(checked) => form.setFieldValue('features.has_bench', checked)}
+            />
+            <CheckboxCard
+              title={'Has Crossing?'}
+              description={'If this stop has a crossing'}
+              checked={form.values.features.has_crossing}
+              onChange={(checked) => form.setFieldValue('features.has_crossing', checked)}
+            />
+            <CheckboxCard
+              title={'Has Flag?'}
+              description={'If this stop has a flag'}
+              checked={form.values.features.has_flag}
+              onChange={(checked) => form.setFieldValue('features.has_flag', checked)}
+            />
+            <CheckboxCard
+              title={'Has Abusive Parking?'}
+              description={'If this stop has abusive parking'}
+              checked={form.values.features.has_abusive_parking}
+              onChange={(checked) => form.setFieldValue('features.has_abusive_parking', checked)}
             />
           </Grid>
-        </Group>
+        </Pannel>
       </PageContainer>
     </form>
   );
