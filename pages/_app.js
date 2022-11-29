521f7db4297b3b3e0158994cc861a4c9cec508bf
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-11-29 19:18:37 +0000

    SWR fetcher function now with error handling

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,13 @@ export default function App({ Component, pageProps }) {
     refreshInterval: 5000,
     fetcher: async (...args) => {
       const res = await fetch(...args);
+      if (!res.ok) {
+        const errorDetails = await res.json();
+        const error = new Error(errorDetails.message || 'An error occurred while fetching data.');
+        error.description = errorDetails.description || 'No additional information was provided by the API.';
+        error.status = res.status;
+        throw error;
+      }
       return res.json();
     },
   };
