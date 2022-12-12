12e0ebef39b300d3afb22dd8e16ab2080ad4de3f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-12-12 11:08:34 +0000

    Suppress deprecation warning

--- services/mongodb.js
+++ services/mongodb.js
@@ -8,6 +8,7 @@ import mongoose from 'mongoose';
 
 async function connect() {
   await mongoose
+    .set('strictQuery', true)
     .connect(process.env.MONGODB_CONNECTION_STRING)
     // .then(() => console.log('Connected.'))
     .catch((error) => {
