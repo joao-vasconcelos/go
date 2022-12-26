6e8cd262580ab36cd480fc3f014fefdc4b85b2ff
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-12-26 16:46:38 +0000

    Fix for ReactDnD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: false, // required for ReactDnD
   // experimental: { appDir: true },
 };
