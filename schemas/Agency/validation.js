bb03c350036e774d0a477f7a0b8b4ef4662c627c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-05-12 00:26:43 +0100

    Removed required fields

--- schemas/Agency/validation.js
+++ schemas/Agency/validation.js
@@ -12,8 +12,8 @@ export const Validation = yup.object({
   agency_name: yup.string().max(100, 'Nome da Agência não deve exceder os ${max} caracteres.'),
   agency_timezone: yup.string().required('Fuso Horário é um campo obrigatório.'),
   agency_lang: yup.string().required('Idioma é um campo obrigatório.'),
-  agency_phone: yup.string().required('Contacto Telefónico é um campo obrigatório.'),
-  agency_email: yup.string().required('Contacto Eletrónico é um campo obrigatório.'),
-  agency_url: yup.string().required('Website é um campo obrigatório.'),
-  agency_fare_url: yup.string().required('Website dos Tarifários é um campo obrigatório.'),
+  agency_phone: yup.string(),
+  agency_email: yup.string(),
+  agency_url: yup.string(),
+  agency_fare_url: yup.string(),
 });
