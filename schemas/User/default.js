11ce9676d7f5b440ed6280b3f1308d967bf020c8
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-05-17 18:25:05 +0100

    Added permissions for users

--- schemas/User/default.js
+++ schemas/User/default.js
@@ -10,4 +10,61 @@ export const Default = {
   email: '',
   phone: '',
   emailVerified: '',
+  permissions: {
+    // AGENCIES
+    agencies_view: false,
+    agencies_create: false,
+    agencies_edit: false,
+    agencies_delete: false,
+    // ALERTS
+    alerts_view: false,
+    alerts_create: false,
+    alerts_edit: false,
+    alerts_delete: false,
+    // CALENDARS
+    calendars_view: false,
+    calendars_create: false,
+    calendars_edit: false,
+    calendars_delete: false,
+    // DATES
+    dates_view: false,
+    dates_create: false,
+    dates_edit: false,
+    dates_delete: false,
+    // FARES
+    fares_view: false,
+    fares_create: false,
+    fares_edit: false,
+    fares_delete: false,
+    // LINES
+    lines_view: false,
+    lines_create: false,
+    lines_edit: false,
+    lines_delete: false,
+    // MUNICIPALITIES
+    municipalities_view: false,
+    municipalities_create: false,
+    municipalities_edit: false,
+    municipalities_delete: false,
+    // SHAPES
+    shapes_view: false,
+    shapes_create: false,
+    shapes_edit: false,
+    shapes_delete: false,
+    // STOPS
+    stops_view: false,
+    stops_create: false,
+    stops_edit: false,
+    stops_delete: false,
+    // THREADS
+    threads_view: false,
+    threads_create: false,
+    threads_edit: false,
+    threads_delete: false,
+    // USERS
+    users_view: false,
+    users_create: false,
+    users_edit: false,
+    users_delete: false,
+  },
 };
--- schemas/User/model.js
+++ schemas/User/model.js
@@ -25,6 +25,174 @@ export const Schema = new mongoose.Schema({
     type: String,
     maxlength: 15,
   },
+  agencies: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Agency',
+    },
+  ],
+  municipalities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Municipalities',
+    },
+  ],
+  permissions: {
+    //
+    // AGENCIES
+    agencies_view: {
+      type: Boolean,
+    },
+    agencies_create: {
+      type: Boolean,
+    },
+    agencies_edit: {
+      type: Boolean,
+    },
+    agencies_delete: {
+      type: Boolean,
+    },
+    //
+    // ALERTS
+    alerts_view: {
+      type: Boolean,
+    },
+    alerts_create: {
+      type: Boolean,
+    },
+    alerts_edit: {
+      type: Boolean,
+    },
+    alerts_delete: {
+      type: Boolean,
+    },
+    //
+    // CALENDARS
+    calendars_view: {
+      type: Boolean,
+    },
+    calendars_create: {
+      type: Boolean,
+    },
+    calendars_edit: {
+      type: Boolean,
+    },
+    calendars_delete: {
+      type: Boolean,
+    },
+    //
+    // DATES
+    dates_view: {
+      type: Boolean,
+    },
+    dates_create: {
+      type: Boolean,
+    },
+    dates_edit: {
+      type: Boolean,
+    },
+    dates_delete: {
+      type: Boolean,
+    },
+    //
+    // FARES
+    fares_view: {
+      type: Boolean,
+    },
+    fares_create: {
+      type: Boolean,
+    },
+    fares_edit: {
+      type: Boolean,
+    },
+    fares_delete: {
+      type: Boolean,
+    },
+    //
+    // LINES
+    lines_view: {
+      type: Boolean,
+    },
+    lines_create: {
+      type: Boolean,
+    },
+    lines_edit: {
+      type: Boolean,
+    },
+    lines_delete: {
+      type: Boolean,
+    },
+    //
+    // MUNICIPALITIES
+    municipalities_view: {
+      type: Boolean,
+    },
+    municipalities_create: {
+      type: Boolean,
+    },
+    municipalities_edit: {
+      type: Boolean,
+    },
+    municipalities_delete: {
+      type: Boolean,
+    },
+    //
+    // SHAPES
+    shapes_view: {
+      type: Boolean,
+    },
+    shapes_create: {
+      type: Boolean,
+    },
+    shapes_edit: {
+      type: Boolean,
+    },
+    shapes_delete: {
+      type: Boolean,
+    },
+    //
+    // STOPS
+    stops_view: {
+      type: Boolean,
+    },
+    stops_create: {
+      type: Boolean,
+    },
+    stops_edit: {
+      type: Boolean,
+    },
+    stops_delete: {
+      type: Boolean,
+    },
+    //
+    // THREADS
+    threads_view: {
+      type: Boolean,
+    },
+    threads_create: {
+      type: Boolean,
+    },
+    threads_edit: {
+      type: Boolean,
+    },
+    threads_delete: {
+      type: Boolean,
+    },
+    //
+    // USERS
+    users_view: {
+      type: Boolean,
+    },
+    users_create: {
+      type: Boolean,
+    },
+    users_edit: {
+      type: Boolean,
+    },
+    users_delete: {
+      type: Boolean,
+    },
+  },
 });
 
 /* * */
