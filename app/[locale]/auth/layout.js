541c3b153c6bd9bede7dcc6a9888650264886b55
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-05-17 20:39:19 +0100

    Remove providers

--- app/[locale]/auth/layout.js
+++ app/[locale]/auth/layout.js
@@ -1,30 +1,16 @@
-'use client';
-
 import styles from './layout.module.css';
-import { MantineProvider } from '@mantine/core';
-import { useColorScheme } from '@mantine/hooks';
 import appBackground from '../../../public/background.jpg';
-import { SessionProvider } from 'next-auth/react';
 import { CMLogo } from '../../../components/AppLogos/AppLogos';
 
-// {children} will be a page or nested layout
-export default function AuthLayout({ children, session }) {
-  //
-
-  const preferredColorScheme = useColorScheme();
-
+export default async function AuthLayout({ children }) {
   return (
-    <SessionProvider session={session}>
-      <MantineProvider theme={{ colorScheme: preferredColorScheme }} withGlobalStyles withNormalizeCSS>
-        <div className={styles.container} style={{ backgroundImage: `url(${appBackground.src})` }}>
-          <div className={styles.loginForm}>
-            <div className={styles.logoWrapper}>
-              <CMLogo />
-            </div>
-            <div className={styles.formWrapper}>{children}</div>
-          </div>
+    <div className={styles.container} style={{ backgroundImage: `url(${appBackground.src})` }}>
+      <div className={styles.loginForm}>
+        <div className={styles.logoWrapper}>
+          <CMLogo />
         </div>
-      </MantineProvider>
-    </SessionProvider>
+        <div className={styles.formWrapper}>{children}</div>
+      </div>
+    </div>
   );
 }
