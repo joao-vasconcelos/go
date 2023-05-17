fbe6792c745acd40bb56a517c5b50a0b2339c3f2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-05-17 18:24:30 +0100

    Log current session

--- app/[locale]/dashboard/page.js
+++ app/[locale]/dashboard/page.js
@@ -1,4 +1,12 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+
 export default function Page() {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
   return (
     <div>
       <h1>Hello, Next.js dash!</h1>
