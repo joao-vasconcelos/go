ac9644417b5ccb8dd6784999d948dc09cb2de342
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-05-25 14:11:23 +0100

    Added validation for Messages

--- schemas/Message/validation.js
+++ schemas/Message/validation.js
@@ -7,4 +7,9 @@ import * as yup from 'yup';
 
 /* * */
 /* A. YUP Validation Schema */
-export const Validation = yup.object({});
+export const Validation = yup.object({
+  thread_id: yup.string(),
+  content: yup.string().required('Please write your message here.').max(1000, ''),
+  sent_by: yup.string(),
+  files: yup.array({ filename: yup.string(), url: yup.string() }),
+});
