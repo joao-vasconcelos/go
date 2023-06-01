630356a90f5c7f2584217b7a12f3d3ee9e766972
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-01 17:35:05 +0100

    Code cleanup

--- components/MessageComponent/MessageComponent.js
+++ components/MessageComponent/MessageComponent.js
@@ -1,6 +1,5 @@
 import styles from './MessageComponent.module.css';
-import { Tooltip, Select, SimpleGrid, TextInput, ActionIcon, Textarea } from '@mantine/core';
-import { IconTrash, IconFileDownload } from '@tabler/icons-react';
+import { IconFileDownload } from '@tabler/icons-react';
 import { useTranslations } from 'next-intl';
 import { useSession } from 'next-auth/react';
 
@@ -10,14 +9,12 @@ export default function MessageComponent({ message }) {
   const { data: session } = useSession();
   const t = useTranslations('threads');
 
-  const currentUser = 'User1';
-
   const messageIsFromCurrentUser = message.sent_by && message.sent_by._id === session?.user?._id;
 
   return (
     <div className={`${styles.container} ${messageIsFromCurrentUser && styles.fromCurrentUser}`}>
       <div className={styles.sentBy}>
-        Sent by <div className={styles.user}>{message.sent_by.name}</div>
+        Sent by <div className={styles.user}>{message.sent_by && message.sent_by.name}</div>
       </div>
       <div className={styles.content}>{message.content}</div>
       <div className={styles.files}>
