09c9a17712cda72a6ea4e7638bb81f06fc9c9f6a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-06 18:04:04 +0100

    Cleanup

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,18 @@
 //
 // ROOT LAYOUT
 
-export const metadata = {
-  title: 'Home',
-  description: 'Welcome to Next.js',
-};
-
 import '../styles/reset.css';
 import '../styles/defaults.css';
 import '../styles/colors.css';
 import { Inter } from 'next/font/google';
 import Providers from './providers';
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-});
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Home',
+  description: 'Welcome to Next.js',
+};
 
 export default function RootLayout({ children }) {
   return (
