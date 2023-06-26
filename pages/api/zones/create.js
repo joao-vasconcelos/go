ea0b92a04af4ff6c1bce49029e5cb14dc1346be9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-06-26 14:48:41 +0100

    Generate random code on create

--- pages/api/zones/create.js
+++ pages/api/zones/create.js
@@ -2,6 +2,7 @@ import delay from '../../../services/delay';
 import mongodb from '../../../services/mongodb';
 import { Default as ZoneDefault } from '../../../schemas/Zone/default';
 import { Model as ZoneModel } from '../../../schemas/Zone/model';
+import generator from '@/services/generator';
 
 /* * */
 /* CREATE ZONE */
@@ -28,7 +29,8 @@ export default async function zonesCreate(req, res) {
 
   // 2. Try to save a new document with req.body
   try {
-    const createdDocument = await ZoneModel(ZoneDefault).save();
+    const newDocument = { ...ZoneDefault, code: generator(5) };
+    const createdDocument = await ZoneModel(newDocument).save();
     return await res.status(201).json(createdDocument);
   } catch (err) {
     console.log(err);
