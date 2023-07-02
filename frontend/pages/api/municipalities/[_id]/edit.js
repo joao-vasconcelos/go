c12347fa93ff41f77c9c4673917e033c7ac137d0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-02 21:34:27 +0100

    Municipalities

--- frontend/pages/api/municipalities/[_id]/edit.js
+++ frontend/pages/api/municipalities/[_id]/edit.js
@@ -1,24 +1,39 @@
-import delay from '../../../../services/delay';
-import mongodb from '../../../../services/mongodb';
-import { Validation as MunicipalityValidation } from '../../../../schemas/Municipality/validation';
-import { Model as MunicipalityModel } from '../../../../schemas/Municipality/model';
+import delay from '@/services/delay';
+import checkAuthentication from '@/services/checkAuthentication';
+import mongodb from '@/services/mongodb';
+import { Validation as MunicipalityValidation } from '@/schemas/Municipality/validation';
+import { Model as MunicipalityModel } from '@/schemas/Municipality/model';
 
 /* * */
 /* EDIT MUNICIPALITY */
 /* Explanation needed. */
 /* * */
 
-export default async function municipalitiesEdit(req, res) {
+export default async function handler(req, res) {
   //
   await delay();
 
-  // 0. Refuse request if not PUT
+  // 0.
+  // Refuse request if not PUT
+
   if (req.method != 'PUT') {
     await res.setHeader('Allow', ['PUT']);
     return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
   }
 
-  // 1. Parse request body into JSON
+  // 1.
+  // Check for correct Authentication and valid Permissions
+
+  try {
+    await checkAuthentication({ scope: 'municipalities', permission: 'create_edit', req, res });
+  } catch (err) {
+    console.log(err);
+    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+  }
+
+  // 2.
+  // Parse request body into JSON
+
   try {
     req.body = await JSON.parse(req.body);
   } catch (err) {
@@ -27,7 +42,9 @@ export default async function municipalitiesEdit(req, res) {
     return;
   }
 
-  // 2. Validate req.body against schema
+  // 3.
+  // Validate req.body against schema
+
   try {
     req.body = MunicipalityValidation.cast(req.body);
   } catch (err) {
@@ -35,7 +52,9 @@ export default async function municipalitiesEdit(req, res) {
     return await res.status(400).json({ message: JSON.parse(err.message)[0].message });
   }
 
-  // 3. Try to connect to mongodb
+  // 4.
+  // Connect to mongodb
+
   try {
     await mongodb.connect();
   } catch (err) {
@@ -43,21 +62,25 @@ export default async function municipalitiesEdit(req, res) {
     return await res.status(500).json({ message: 'MongoDB connection error.' });
   }
 
-  // 4. Check for uniqueness
+  // 5.
+  // Check for uniqueness
+
   try {
     // The values that need to be unique are ['code'].
-    const foundDocumentWithMunicipalityCode = await MunicipalityModel.exists({ code: req.body.code });
-    if (foundDocumentWithMunicipalityCode && foundDocumentWithMunicipalityCode._id != req.query._id) {
-      throw new Error('Um Município com o mesmo Código já existe.');
+    const foundDocumentWithCode = await MunicipalityModel.exists({ code: { $eq: req.body.code } });
+    if (foundDocumentWithCode && foundDocumentWithCode._id != req.query._id) {
+      throw new Error('Um Tarifário com o mesmo Código já existe.');
     }
   } catch (err) {
     console.log(err);
     return await res.status(409).json({ message: err.message });
   }
 
-  // 2. Try to update the correct document
+  // 6.
+  // Update the requested document
+
   try {
-    const editedDocument = await MunicipalityModel.findOneAndUpdate({ _id: req.query._id }, req.body, { new: true });
+    const editedDocument = await MunicipalityModel.findOneAndUpdate({ _id: { $eq: req.query._id } }, req.body, { new: true });
     if (!editedDocument) return await res.status(404).json({ message: `Municipality with _id: ${req.query._id} not found.` });
     return await res.status(200).json(editedDocument);
   } catch (err) {
--- frontend/pages/api/municipalities/create.js
+++ frontend/pages/api/municipalities/create.js
@@ -1,4 +1,5 @@
 import delay from '@/services/delay';
+import checkAuthentication from '@/services/checkAuthentication';
 import mongodb from '@/services/mongodb';
 import generator from '@/services/generator';
 import { Default as MunicipalityDefault } from '@/schemas/Municipality/default';
@@ -9,17 +10,31 @@ import { Model as MunicipalityModel } from '@/schemas/Municipality/model';
 /* Explanation needed. */
 /* * */
 
-export default async function municipalitiesCreate(req, res) {
+export default async function handler(req, res) {
   //
   await delay();
 
-  // 0. Refuse request if not GET
+  // 0.
+  // Refuse request if not GET
+
   if (req.method != 'GET') {
     await res.setHeader('Allow', ['GET']);
     return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
   }
 
-  // 1. Try to connect to mongodb
+  // 1.
+  // Check for correct Authentication and valid Permissions
+
+  try {
+    await checkAuthentication({ scope: 'municipalities', permission: 'create_edit', req, res });
+  } catch (err) {
+    console.log(err);
+    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+  }
+
+  // 2.
+  // Connect to MongoDB
+
   try {
     await mongodb.connect();
   } catch (err) {
@@ -27,14 +42,15 @@ export default async function municipalitiesCreate(req, res) {
     return await res.status(500).json({ message: 'MongoDB connection error.' });
   }
 
-  // 2. Try to save a new document with req.body
+  // 3.
+  // Save a new document with default values
+
   try {
-    const newDocument = { ...MunicipalityDefault, code: generator(4, 'numeric') };
+    const newDocument = { ...MunicipalityDefault, code: generator(5) };
     while (await MunicipalityModel.exists({ code: newDocument.code })) {
-      newDocument.code = generator(4, 'numeric');
+      newDocument.code = generator(5);
     }
-    const createdDocument = await MunicipalityModel(newDocument).save();
-    return await res.status(201).json(createdDocument);
+    return await res.status(201).json(newDocument);
   } catch (err) {
     console.log(err);
     return await res.status(500).json({ message: 'Cannot create this Municipality.' });
