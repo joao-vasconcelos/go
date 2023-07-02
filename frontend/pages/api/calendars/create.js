07fa6f205780db0a6a1fdbd403a04f21113f4cb9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-02 01:15:19 +0100

    Check auth Calendars

--- frontend/pages/api/calendars/create.js
+++ frontend/pages/api/calendars/create.js
@@ -1,24 +1,39 @@
-import delay from '../../../services/delay';
-import mongodb from '../../../services/mongodb';
-import { Default as CalendarDefault } from '../../../schemas/Calendar/default';
-import { Model as CalendarModel } from '../../../schemas/Calendar/model';
+import delay from '@/services/delay';
+import checkAuthentication from '@/services/checkAuthentication';
+import mongodb from '@/services/mongodb';
+import { Default as CalendarDefault } from '@/schemas/Calendar/default';
+import { Model as CalendarModel } from '@/schemas/Calendar/model';
 
 /* * */
 /* CREATE CALENDAR */
 /* Explanation needed. */
 /* * */
 
-export default async function calendarsCreate(req, res) {
+export default async function handler(req, res) {
   //
   await delay();
 
-  // 0. Refuse request if not GET
+  // 0.
+  // Refuse request if not GET
+
   if (req.method != 'GET') {
     await res.setHeader('Allow', ['GET']);
     return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
   }
 
-  // 1. Try to connect to mongodb
+  // 1.
+  // Check for correct Authentication and valid Permissions
+
+  try {
+    await checkAuthentication({ scope: 'calendars', permission: 'create_edit', req, res });
+  } catch (err) {
+    console.log(err);
+    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+  }
+
+  // 2.
+  // Connect to MongoDB
+
   try {
     await mongodb.connect();
   } catch (err) {
@@ -26,7 +41,9 @@ export default async function calendarsCreate(req, res) {
     return await res.status(500).json({ message: 'MongoDB connection error.' });
   }
 
-  // 2. Try to save a new document with req.body
+  // 3.
+  // Save a new document with req.body
+
   try {
     const createdDocument = await CalendarModel(CalendarDefault).save();
     return await res.status(201).json(createdDocument);
--- frontend/pages/api/typologies/[_id]/index.js
+++ frontend/pages/api/typologies/[_id]/index.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
   // Check for correct Authentication and valid Permissions
 
   try {
-    await checkAuthentication({ scope: 'zones', permission: 'view', req, res });
+    await checkAuthentication({ scope: 'typologies', permission: 'view', req, res });
   } catch (err) {
     console.log(err);
     return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
