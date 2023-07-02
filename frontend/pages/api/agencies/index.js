839fc24fa8a6e178746312b1e587d5f5c2b04d49
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-02 04:09:10 +0100

    Fares auth

--- frontend/pages/api/agencies/index.js
+++ frontend/pages/api/agencies/index.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
   // List all documents
 
   try {
-    const allDocuments = await AgencyModel.find({});
+    const allDocuments = await AgencyModel.find();
     return await res.status(200).send(allDocuments);
   } catch (err) {
     console.log(err);
--- frontend/pages/api/fares/[_id]/delete.js
+++ frontend/pages/api/fares/[_id]/delete.js
@@ -7,17 +7,31 @@ import { Model as FareModel } from '../../../../schemas/Fare/model';
 /* Explanation needed. */
 /* * */
 
-export default async function faresDelete(req, res) {
+export default async function handler(req, res) {
   //
   await delay();
 
-  // 0. Refuse request if not DELETE
+  // 0.
+  // Refuse request if not DELETE
+
   if (req.method != 'DELETE') {
     await res.setHeader('Allow', ['DELETE']);
     return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
   }
 
-  // 1. Try to connect to mongodb
+  // 1.
+  // Check for correct Authentication and valid Permissions
+
+  try {
+    await checkAuthentication({ scope: 'fares', permission: 'delete', req, res });
+  } catch (err) {
+    console.log(err);
+    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+  }
+
+  // 2.
+  // Connect to MongoDB
+
   try {
     await mongodb.connect();
   } catch (err) {
@@ -25,9 +39,11 @@ export default async function faresDelete(req, res) {
     return await res.status(500).json({ message: 'MongoDB connection error.' });
   }
 
-  // 2. Try to update the correct document
+  // 3.
+  // Delete the requested document
+
   try {
-    const deletedDocument = await FareModel.findOneAndDelete({ _id: req.query._id });
+    const deletedDocument = await FareModel.findOneAndDelete({ _id: { $eq: req.query._id } });
     if (!deletedDocument) return await res.status(404).json({ message: `Fare with _id: ${req.query._id} not found.` });
     return await res.status(200).send(deletedDocument);
   } catch (err) {
