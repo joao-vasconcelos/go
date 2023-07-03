e3d2f1e12fd50899f2497f389c3462b95d46842b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-03 16:23:05 +0100

    Update backupd.sh

--- backupd/backupd.sh
+++ backupd/backupd.sh
@@ -3,8 +3,7 @@
 # Infinite loop to run mongodump every minute
 while true; do
   # Run mongodump command
-  mongodump --uri="$MONGODB_CONNECTION_STRING" --out="/backups"
-
-  # Sleep for 1 minute
-  sleep 60
+  mongodump --uri="$MONGODB_CONNECTION_STRING" --archive="go-backup-$(date +\%Y\%m\%d\%H\%M\%S)" #--out="/backups"
+  # Sleep for 30 minutes (1800 seconds)
+  sleep 1800
 done
