bfdd08b4d96eecef88779ad39fb0ac0d3e8e0fd3
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-03 16:02:07 +0100

    Remove crontab

---
+++ backupd/backupd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Define the backup directory where the dumps will be stored
+backup_dir="/backups"
+
+# Define the command to run mongodump
+mongodump_cmd="mongodump --host $MONGODB_CONNECTION_STRING --out $backup_dir"
+
+# Infinite loop to run mongodump every minute
+while true; do
+  # Run mongodump command
+  $mongodump_cmd
+
+  # Sleep for 1 minute
+  sleep 60
+done
