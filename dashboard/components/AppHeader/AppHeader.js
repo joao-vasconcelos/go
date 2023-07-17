d3419d5faff912eed8862beb9d12802f05f62bc2
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-18 00:15:09 +0100

    Nice touch

--- dashboard/components/AppHeader/AppHeader.js
+++ dashboard/components/AppHeader/AppHeader.js
@@ -6,9 +6,11 @@ export default function AppHeader() {
   //
   const { data: session, status } = useSession();
 
+  const greetings = ['Olá', 'Hi', 'Hey', 'Oi', 'Hallo', 'Привіт', 'Ciao', 'Hej'];
+
   return (
     <div className={styles.container}>
-      <p className={styles.greeting}>{`Olá ${(session && session.user.name) || '-'}!`}</p>
+      <p className={styles.greeting}>{session ? `${greetings[(greetings.length * Math.random()) | 0]} ${session.user?.name}` : '• • •'}</p>
       <div className={styles.options}>
         <AppOptions />
       </div>
