16c2eba38d3fc4b4e6a130aea72191d2ebd17a92
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-17 23:50:09 +0100

    AuthGate

--- dashboard/app/[locale]/dashboard/exports/page.js
+++ dashboard/app/[locale]/dashboard/exports/page.js
@@ -1,12 +1,12 @@
+import AuthGate from '@/components/AuthGate/AuthGate';
 import { TwoEvenColumns } from '@/components/Layouts/Layouts';
 import ExportedFilesList from '@/components/ExportedFilesList/ExportedFilesList';
 import ExportFileForm from '@/components/ExportFileForm/ExportFileForm';
 
 export default function Page() {
-  //
-
-  //
-  // E. Render components
-
-  return <TwoEvenColumns first={<ExportedFilesList />} second={<ExportFileForm />} />;
+  return (
+    <AuthGate scope='exports' permission='view' redirect>
+      <TwoEvenColumns first={<ExportedFilesList />} second={<ExportFileForm />} />;
+    </AuthGate>
+  );
 }
