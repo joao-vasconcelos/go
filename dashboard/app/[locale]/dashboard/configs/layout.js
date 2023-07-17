222afdfcbdd0e7349862bc007d7a86ada24c551c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-17 23:52:09 +0100

    Update layout.js

--- dashboard/app/[locale]/dashboard/configs/layout.js
+++ dashboard/app/[locale]/dashboard/configs/layout.js
@@ -1,66 +1,7 @@
-'use client';
-
-import { useState } from 'react';
-import { useRouter } from 'next-intl/client';
-import useSearch from '@/hooks/useSearch';
-import useSWR from 'swr';
-import API from '@/services/API';
-import { OneFullColumn, TwoUnevenColumns } from '@/components/Layouts/Layouts';
-import Pannel from '@/components/Pannel/Pannel';
-import ListItem from './listItem';
-import { ActionIcon, Menu } from '@mantine/core';
-import { IconCirclePlus, IconDots } from '@tabler/icons-react';
-import notify from '@/services/notify';
-import NoDataLabel from '@/components/NoDataLabel/NoDataLabel';
-import ErrorDisplay from '@/components/ErrorDisplay';
-import { useTranslations } from 'next-intl';
-import ListFooter from '@/components/ListFooter/ListFooter';
+import { OneFullColumn } from '@/components/Layouts/Layouts';
 import AuthGate from '@/components/AuthGate/AuthGate';
-import SearchField from '@/components/SearchField/SearchField';
 
 export default function Layout({ children }) {
-  //
-
-  //
-  // A. Setup variables
-
-  const router = useRouter();
-  const t = useTranslations('agencies');
-  const [searchQuery, setSearchQuery] = useState('');
-  const [isCreating, setIsCreating] = useState(false);
-
-  //
-  // B. Fetch data
-
-  const { data: allAgenciesData, error: allAgenciesError, isLoading: allAgenciesLoading, isValidating: allAgenciesValidating, mutate: allAgenciesMutate } = useSWR('/api/agencies');
-
-  //
-  // C. Search
-
-  const filteredAgenciesData = useSearch(searchQuery, allAgenciesData, { keys: ['name', 'code'] });
-
-  //
-  // C. Handle actions
-
-  const handleCreate = async () => {
-    try {
-      setIsCreating(true);
-      notify('new', 'loading', t('operations.create.loading'));
-      const response = await API({ service: 'agencies', operation: 'create', method: 'GET' });
-      allAgenciesMutate();
-      router.push(`/dashboard/agencies/${response._id}`);
-      notify('new', 'success', t('operations.create.success'));
-      setIsCreating(false);
-    } catch (err) {
-      notify('new', 'error', err.message || t('operations.create.error'));
-      setIsCreating(false);
-      console.log(err);
-    }
-  };
-
-  //
-  // D. Render data
-
   return (
     <AuthGate scope='configs' permission='admin' redirect>
       <OneFullColumn first={children} />
