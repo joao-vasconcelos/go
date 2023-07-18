4c93ce921cc1d1dfb49c54372be008d38b6c454f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-18 01:44:11 +0100

    Fix parser

--- dashboard/app/[locale]/dashboard/agencies/[agency_id]/page.js
+++ dashboard/app/[locale]/dashboard/agencies/[agency_id]/page.js
@@ -192,7 +192,7 @@ export default function Page() {
               step={0.01}
               stepHoldDelay={500}
               stepHoldInterval={100}
-              parser={(value) => (isNaN(Number(value)) ? 0 : Number(value))}
+              parser={(value) => value.replace(/\€\s?|(,*)/g, '')}
               formatter={(value) => (!Number.isNaN(parseFloat(value)) ? `€ ${value}`.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') : '€ ')}
               readOnly={isReadOnly}
             />
