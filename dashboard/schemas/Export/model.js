13b8160361db7ba74c8a7037aa5c140f71881025
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-20 12:26:54 +0100

    Updated schemas

--- dashboard/schemas/Export/model.js
+++ dashboard/schemas/Export/model.js
@@ -7,37 +7,35 @@ import mongoose from 'mongoose';
 
 /* * */
 /* A. Mongoose Schema */
-export const ExportSchema = new mongoose.Schema({
-  type: {
-    type: Number,
+export const ExportSchema = new mongoose.Schema(
+  {
+    type: {
+      type: Number,
+    },
+    status: {
+      type: Number,
+    },
+    exported_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    progress_current: {
+      type: Number,
+    },
+    progress_total: {
+      type: Number,
+    },
+    filename: {
+      type: String,
+      maxlength: 50,
+    },
+    workdir: {
+      type: String,
+      maxlength: 200,
+    },
   },
-  status: {
-    type: Number,
-  },
-  exported_by: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-  },
-  progress_current: {
-    type: Number,
-  },
-  progress_total: {
-    type: Number,
-  },
-  filename: {
-    type: String,
-    maxlength: 50,
-  },
-  workdir: {
-    type: String,
-    maxlength: 200,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 3600, // Auto remove document after x seconds have passed from createdAt value
-  },
-});
+  { timestamps: true }
+);
 
 /* * */
 /* C. Mongoose Model */
