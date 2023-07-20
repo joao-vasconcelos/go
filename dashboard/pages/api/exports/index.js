6aea98f9404095e9b0e66b01939f50ddd60da5b9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-20 10:22:44 +0100

    Keep all directories even if they errored

--- dashboard/pages/api/exports/index.js
+++ dashboard/pages/api/exports/index.js
@@ -76,14 +76,14 @@ export default async function handler(req, res) {
 
     // 3.7.
     // Filter Export documents to keep only the ones that are not errors
-    const validExportDocuments = allDocuments.filter((item) => item.status !== 5).map((item) => String(item._id));
+    const validExportDocumentIds = allDocuments.map((item) => String(item._id)); // .filter((item) => item.status !== 5)
 
     // 3.8.
     // Compare the existing files with each document
     // and remove the dangling files from the directory.
     for (const savedExport of savedExportFiles) {
       // Skip if the object matches a document in the database
-      if (validExportDocuments.includes(savedExport.name)) continue;
+      if (validExportDocumentIds.includes(savedExport.name)) continue;
       // Remove the object otherwise
       fs.rmSync(`${workdir}/${savedExport.name}`, { recursive: true, force: true });
     }
