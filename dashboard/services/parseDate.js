3630250249936c26dc18111af59380a9260d4ba9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-26 16:05:04 +0100

    Fix error parsing date

--- dashboard/services/parseDate.js
+++ dashboard/services/parseDate.js
@@ -1,5 +1,7 @@
 export default function parseDate(date, returnType = 'string') {
   //
+  if (!date) return null;
+
   const year = date.getFullYear().toString();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
