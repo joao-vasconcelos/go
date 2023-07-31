863df2e1f84f87a037030558c33823410425a98c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-07-31 22:28:24 +0100

    Cleanup update routes

--- dashboard/pages/api/configs/imports/routes.js
+++ dashboard/pages/api/configs/imports/routes.js
@@ -42,53 +42,91 @@ export default async function handler(req, res) {
   }
 
   // 5.
-  // Connect to mongodb
+  // Ensure latest schema modifications are applied in the database.
+
+  try {
+    await RouteModel.syncIndexes();
+  } catch (err) {
+    console.log(err);
+    return await res.status(500).json({ message: 'Cannot sync indexes.' });
+  }
+
+  // 6.
+  // Update routes
 
   try {
     //
-    // Fetch all Lines from database
+
+    // 6.1.
+    // Retrieve all Lines from database
     const allLines = await LineModel.find({});
 
-    // Get route info for each line
-    for (const line of allLines) {
+    // 6.2.
+    // Iterate through each available line
+    for (const lineData of allLines) {
       //
-      // Skip if not A1
-      //   if (!line.code.startsWith('2')) continue;
 
-      // Get info for the route from API v1
-      const response = await fetch(`https://schedules.carrismetropolitana.pt/api/routes/route_short_name/${line.short_name}`);
-      const allRoutesApiForThisLine = await response.json();
+      // 6.2.0.
+      // Skip if this line is for A4
+      if (lineData.code.startsWith('4')) continue;
 
-      // Parse and add each route to the database
+      // 6.2.1.
+      // Fetch routes for this line from API v1
+      const allRoutesRes = await fetch(`https://schedules.carrismetropolitana.pt/api/routes/route_short_name/${lineData.short_name}`);
+      const allRoutesApi = await allRoutesRes.json();
+
+      // 6.2.2.
+      // Setup a temporary variable to hold created route_ids
       let createdRoutesIds = [];
-      for (const routeApi of allRoutesApiForThisLine) {
+
+      // 6.2.3.
+      // Parse each route
+      for (const routeApi of allRoutesApi) {
         // Parse the route object
         const parsedRoute = {
           code: routeApi.route_id,
           name: routeApi.route_long_name,
           path_type: 1,
-          parent_line: line._id,
+          parent_line: lineData._id,
           patterns: [],
         };
         // Save the route to the database
         const createdRouteDocument = await RouteModel.findOneAndUpdate({ code: parsedRoute.code }, parsedRoute, { new: true, upsert: true });
+        // Save the route_id to the variable
         createdRoutesIds.push(createdRouteDocument._id);
-        console.log(`Saved Route ${parsedRoute.code}`);
+        // Log progress
+        console.log(`⤷ - Saved Route ${parsedRoute.code}`);
+        //
       }
 
-      // Add the created Route objects to the current line, and save the updated document
-      line.routes = createdRoutesIds;
-      await line.save();
+      // 6.2.4.
+      // Add the created Route documents to the current line
+      lineData.routes = createdRoutesIds;
 
-      console.log(`Updated Line ${line.code}`);
-      await delay(250); // 250 miliseconds of delay
+      // 6.2.5.
+      // Save the updated document
+      await lineData.save();
+
+      // 6.2.6.
+      // Log progress
+      console.log(`⤷ Updated Line ${lineData.code}`);
+      console.log();
+
+      // 6.2.7.
+      // Wait for 250 miliseconds to ensure no rate limits are hit
+      await delay(250);
+
       //
     }
+
+    //
   } catch (err) {
     console.log(err);
     return await res.status(500).json({ message: 'Import Error' });
   }
 
-  console.log('Done. Sending response to client...');
+  // 7.
+  // Log progress
+  console.log('⤷ Done. Sending response to client...');
   return await res.status(200).json('Import complete.');
 }
