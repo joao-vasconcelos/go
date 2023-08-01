e92d01b09140793fb01b86ec005793cf3a71242b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-01 12:55:26 +0100

    Afetação A1 (again)

--- dashboard/pages/api/configs/imports/afetacao.js
+++ dashboard/pages/api/configs/imports/afetacao.js
@@ -1,7 +1,7 @@
 import delay from '@/services/delay';
 import checkAuthentication from '@/services/checkAuthentication';
 import mongodb from '@/services/mongodb';
-import afetacaoData from '@/services/afetacao/afetacao_a4_parsed.json';
+import afetacaoData from '@/services/afetacao/afetacao_a1_parsed.json';
 import { PatternModel } from '@/schemas/Pattern/model';
 import { Model as StopModel } from '@/schemas/Stop/model';
 import { Model as ZoneModel } from '@/schemas/Zone/model';
@@ -70,10 +70,10 @@ export default async function handler(req, res) {
 
       // 6.2.0.
       // Skip if this pattern is not for the right area
-      if (patternSummaryData.code.startsWith('1')) continue;
+      //   if (patternSummaryData.code.startsWith('1')) continue;
       if (patternSummaryData.code.startsWith('2')) continue;
       if (patternSummaryData.code.startsWith('3')) continue;
-      //   if (patternSummaryData.code.startsWith('4')) continue;
+      if (patternSummaryData.code.startsWith('4')) continue;
 
       const patternData = await PatternModel.findOne({ code: patternSummaryData.code }).populate('path.stop');
 
