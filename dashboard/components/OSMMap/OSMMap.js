a16429d61d5050799547d17d24066a18aa9c8b28
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-31 11:10:41 +0100

    More options for OSM Map

--- dashboard/components/OSMMap/OSMMap.js
+++ dashboard/components/OSMMap/OSMMap.js
@@ -1,11 +1,11 @@
 import styles from './OSMMap.module.css';
 import osmMapDefaults from './OSMMap.config';
-import Map, { NavigationControl, FullscreenControl } from 'react-map-gl/maplibre';
+import Map, { NavigationControl, FullscreenControl, ScaleControl } from 'react-map-gl/maplibre';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { Divider } from '@mantine/core';
 
-export default function OSMMap({ id, mapStyle, width, height, scrollZoom = true, onClick = () => {}, interactiveLayerIds = [], children, toolbar }) {
+export default function OSMMap({ id, mapStyle, width, height, scrollZoom = true, onClick = () => {}, interactiveLayerIds = [], children, toolbar, navigation = true, fullscreen = true, scale = true }) {
   return (
     <div className={styles.container} style={{ width: width || '100%', height: height || '100%' }}>
       <Map
@@ -21,8 +21,9 @@ export default function OSMMap({ id, mapStyle, width, height, scrollZoom = true,
         interactive={interactiveLayerIds ? true : false}
         interactiveLayerIds={interactiveLayerIds}
       >
-        <NavigationControl />
-        <FullscreenControl />
+        {navigation && <NavigationControl />}
+        {fullscreen && <FullscreenControl />}
+        {scale && <ScaleControl maxWidth={100} unit="metric" />}
         {children}
       </Map>
       <Divider />
