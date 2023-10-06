f12a5f89249ea9fac5b8111ca08873f8648319c4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-06 20:02:52 +0100

    Better logs

--- dashboard/services/checkAuthentication.js
+++ dashboard/services/checkAuthentication.js
@@ -20,7 +20,7 @@ export default async function checkAuthentication({ scope, permission, req, res 
   // 1.2.
   // Check if the current user has permission to access the feature
   if (!session?.user?.permissions[scope][permission] === true) {
-    throw new Error('You do not have permission to access this feature.', scope, permission);
+    throw new Error(`Permission denied for user "${session?.user?.name}" | Scope: ${scope} | Permission: ${permission}`);
   }
 
   // 1.3.
