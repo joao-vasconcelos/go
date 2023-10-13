c0e0c8521106db641b837550c97f41bc931da0bc
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-13 20:32:00 +0100

    Standardize more calendars, only for A3

--- dashboard/pages/api/configs/refactors/standardizeCalendars.js
+++ dashboard/pages/api/configs/refactors/standardizeCalendars.js
@@ -52,7 +52,7 @@ export default async function handler(req, res) {
     // For each pattern
     for (const patternCode of allPatternCodes) {
       //
-      //   if (!patternCode.code.startsWith('1')) continue;
+      if (!patternCode.code.startsWith('3')) continue;
       //
       const patternData = await PatternModel.findOne({ code: patternCode.code });
 
@@ -80,6 +80,12 @@ export default async function handler(req, res) {
         let standardizedCalendars = new Set();
 
         /* * * * * * * * * */
+
+        //
+        if (associatedCalendarCodes.has('50')) {
+          standardizedCalendars.add('CM_FER_DU');
+          associatedCalendarCodes.delete('50');
+        }
 
         //
         if (associatedCalendarCodes.has('A1_0_1')) {
