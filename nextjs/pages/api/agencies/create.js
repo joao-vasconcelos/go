814cf14c40440bd4e096e688573d09234ed33fff
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-02-22 12:07:54 +0000

    Create agencies

--- nextjs/pages/api/agencies/create.js
+++ nextjs/pages/api/agencies/create.js
@@ -1,8 +1,7 @@
 /* * */
 
-import mongodb from '@/services/mongodb';
 import getSession from '@/authentication/getSession';
-import isAllowed from '@/authentication/isAllowed';
+import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 import generator from '@/services/generator';
 import { AgencyDefault } from '@/schemas/Agency/default';
 import { AgencyModel } from '@/schemas/Agency/model';
@@ -18,35 +17,26 @@ export default async function handler(req, res) {
   let sessionData;
 
   // 2.
-  // Refuse request if not GET
-
-  if (req.method != 'GET') {
-    await res.setHeader('Allow', ['GET']);
-    return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
-  }
-
-  // 3.
-  // Check for correct Authentication and valid Permissions
+  // Get session data
 
   try {
     sessionData = await getSession(req, res);
-    isAllowed(sessionData, [{ scope: 'agencies', action: 'create' }]);
   } catch (err) {
     console.log(err);
-    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+    return await res.status(400).json({ message: err.message || 'Could not get Session data. Are you logged in?' });
+  }
+
+  // 3.
+  // Prepare endpoint
+
+  try {
+    await prepareApiEndpoint({ request: req, method: 'GET', session: sessionData, permissions: [{ scope: 'agencies', action: 'create' }] });
+  } catch (err) {
+    console.log(err);
+    return await res.status(400).json({ message: err.message || 'Could not prepare endpoint.' });
   }
 
   // 4.
-  // Connect to MongoDB
-
-  try {
-    await mongodb.connect();
-  } catch (err) {
-    console.log(err);
-    return await res.status(500).json({ message: 'MongoDB connection error.' });
-  }
-
-  // 5.
   // Save a new document with default values
 
   try {
