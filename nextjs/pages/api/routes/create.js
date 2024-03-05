30d11b6e2ab257e98e03b31d8b1641bc54d432a7
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-03-05 20:47:32 +0100

    Fix route create endpoint

--- nextjs/pages/api/routes/create.js
+++ nextjs/pages/api/routes/create.js
@@ -1,8 +1,7 @@
 /* * */
 
-import mongodb from '@/services/mongodb';
 import getSession from '@/authentication/getSession';
-import isAllowed from '@/authentication/isAllowed';
+import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 import generator from '@/services/generator';
 import { RouteDefault } from '@/schemas/Route/default';
 import { RouteModel } from '@/schemas/Route/model';
@@ -19,32 +18,23 @@ export default async function handler(req, res) {
   let sessionData;
 
   // 2.
-  // Refuse request if not GET
-
-  if (req.method != 'GET') {
-    await res.setHeader('Allow', ['GET']);
-    return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
-  }
-
-  // 3.
-  // Check for correct Authentication and valid Permissions
+  // Get session data
 
   try {
     sessionData = await getSession(req, res);
-    isAllowed(sessionData, [{ scope: 'routes', action: 'create' }]);
   } catch (err) {
     console.log(err);
-    return await res.status(401).json({ message: err.message || 'Could not verify Authentication.' });
+    return await res.status(400).json({ message: err.message || 'Could not get Session data. Are you logged in?' });
   }
 
-  // 4.
-  // Connect to MongoDB
+  // 3.
+  // Prepare endpoint
 
   try {
-    await mongodb.connect();
+    await prepareApiEndpoint({ request: req, method: 'POST', session: sessionData, permissions: [{ scope: 'lines', action: 'edit' }] });
   } catch (err) {
     console.log(err);
-    return await res.status(500).json({ message: 'MongoDB connection error.' });
+    return await res.status(400).json({ message: err.message || 'Could not prepare endpoint.' });
   }
 
   // 5.
