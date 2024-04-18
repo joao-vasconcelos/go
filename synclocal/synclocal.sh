e982e38a8c7bca722ee6d4b2a2fac2d9d8268905
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-19 00:19:07 +0100

    Update synclocal.sh

--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -2,5 +2,5 @@
 
 # Restore the database from backup
 echo "Starting restore..."
-mongorestore --uri="mongodb://mongodbuser:mongodbpassword@mongodb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-20240411234302"
+mongorestore --uri="mongodb://mongodbuser:mongodbpassword@mongodb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-20240418174517"
 echo "Restore complete!"
