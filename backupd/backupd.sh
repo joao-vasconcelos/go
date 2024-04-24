b59dfedbadccc193ad5360e7a621dc311794ac5c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-24 19:26:09 +0100

    Restore from backup

--- backupd/backupd.sh
+++ backupd/backupd.sh
@@ -15,22 +15,22 @@ files_to_keep=300
 while true; do
 
     # Backup the database to an archive
-    echo "Starting backup..."
-    mongodump --uri="$MONGODB_URI" --gzip --archive="$directory/go-backup-$(date +\%Y\%m\%d\%H\%M\%S)"
-    echo "Backup complete!"
+    # echo "Starting backup..."
+    # mongodump --uri="$MONGODB_URI" --gzip --archive="$directory/go-backup-$(date +\%Y\%m\%d\%H\%M\%S)"
+    # echo "Backup complete!"
 
     # Restore the database from backup
-    # echo "Starting restore..."
-    # mongorestore --uri="$MONGODB_URI" --drop --preserveUUID --gzip --archive="$directory/go-backup-20240112153325"
-    # echo "Restore complete!"
+    echo "Starting restore..."
+    mongorestore --uri="$MONGODB_URI" --drop --preserveUUID --gzip --archive="$directory/go-backup-20240424180102"
+    echo "Restore complete!"
 
     # Only keep the most recent files
     echo "Preparing to remove older backups. Will only keep the $files_to_keep most recent backup archives."
-    
+
     # Sort the files by modification time (most recent first) and count how many were found
-    sorted_files=$(find "$directory" -maxdepth 1 -type f -printf "%p\n" | sort -r -k1 | awk '{print $1}') 
+    sorted_files=$(find "$directory" -maxdepth 1 -type f -printf "%p\n" | sort -r -k1 | awk '{print $1}')
     files_count=$(echo "$sorted_files" | wc -l)
-    
+
     # Delete the extra files if the count exceeds the threshold
     if [ "$files_count" -gt "$files_to_keep" ]; then
         files_to_delete=$(echo "$sorted_files" | tail -n +$((files_to_keep + 1)))
