bd62781d84372ef71d11c398b6f9b645cb9d87b4
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-25 00:06:35 +0100

    Cleanup repo, renamed worker modules

---
+++ bump-version.js
@@ -0,0 +1,24 @@
+/* * */
+
+const fs = require('fs');
+
+/* * */
+
+const now = new Date();
+const year = now.getFullYear();
+const month = padNumber(now.getMonth() + 1);
+const day = padNumber(now.getDate());
+const hours = padNumber(now.getHours());
+const minutes = padNumber(now.getMinutes());
+
+const newVersion = `${year}.${month}.${day}-${hours}${minutes}`;
+
+fs.writeFileSync('./nextjs/package.json', JSON.stringify({ ...require('./nextjs/package.json'), version: newVersion }, null, 2));
+fs.writeFileSync('./slamanager-feeder/package.json', JSON.stringify({ ...require('./slamanager-feeder/package.json'), version: newVersion }, null, 2));
+fs.writeFileSync('./slamanager-monitor/package.json', JSON.stringify({ ...require('./slamanager-monitor/package.json'), version: newVersion }, null, 2));
+
+/* * */
+
+function padNumber(number) {
+  return number.toString().padStart(2, '0');
+}
---
+++ slamanager-monitor/services/SLAMANAGERDB.js
@@ -0,0 +1,155 @@
+/* * */
+
+const { MongoClient } = require('mongodb');
+
+/* * */
+
+const MAX_CONNECTION_RETRIES = 1;
+
+/* * */
+
+class SLAMANAGERDB {
+  //
+
+  constructor() {
+    //
+    this.mongoClientConnecting = false;
+    this.mongoClientConnectionRetries = 0;
+    this.mongoClientConnectionInstance = null;
+    //
+  }
+
+  /* * *
+   * CONNECT
+   * This function sets up a MongoDB client instance with the necessary databases and collections.
+   */
+
+  async connect() {
+    try {
+      console.log('SLAMANAGERDB: New connection request...');
+
+      //
+      // If another connection request is already in progress, wait for it to complete
+
+      if (this.mongoClientConnecting) {
+        console.log('SLAMANAGERDB: Waiting for MongoDB Client connection...');
+        await this.waitForMongoClientConnection();
+        return;
+      }
+
+      //
+      // Setup the flag to prevent double connection
+
+      this.mongoClientConnecting = true;
+
+      //
+      // Setup MongoDB connection options
+
+      const mongoClientOptions = {
+        minPoolSize: 2,
+        maxPoolSize: 200,
+        directConnection: true,
+        // readPreference: 'secondaryPreferred',
+        connectTimeoutMS: 5000,
+        serverSelectionTimeoutMS: 5000,
+      };
+
+      //
+      // Create the client instance
+
+      let mongoClientInstance;
+
+      //
+      // Check if there is already an active MongoDB Client connection
+
+      if (this.mongoClientConnectionInstance && this.mongoClientConnectionInstance.topology && this.mongoClientConnectionInstance.topology.isConnected()) {
+        mongoClientInstance = this.mongoClientConnectionInstance;
+      } else if (global._mongoClientConnectionInstance && global._mongoClientConnectionInstance.topology && global._mongoClientConnectionInstance.topology.isConnected()) {
+        mongoClientInstance = global._mongoClientConnectionInstance;
+      } else {
+        mongoClientInstance = await MongoClient.connect(process.env.SLAMANAGERDB_MONGODB_URI, mongoClientOptions);
+      }
+
+      //
+      // Setup databases
+
+      const productionDatabase = mongoClientInstance.db('production');
+
+      //
+      // Setup collections
+
+      this.Trip = productionDatabase.collection('Trip');
+      this.Shape = productionDatabase.collection('Shape');
+
+      //
+      // Setup indexes
+
+      this.Trip.createIndex({ code: 1 }, { unique: true });
+      this.Trip.createIndex({ status: 1 });
+      this.Trip.createIndex({ operational_day: 1 });
+      this.Trip.createIndex({ agency_id: 1 });
+      this.Trip.createIndex({ plan_id: 1 });
+      this.Trip.createIndex({ trip_id: 1 });
+      this.Trip.createIndex({ line_id: 1 });
+      this.Trip.createIndex({ pattern_id: 1 });
+
+      this.Shape.createIndex({ code: 1 }, { unique: true });
+      this.Shape.createIndex({ shape_id: 1 });
+
+      //
+      // Save the instance in memory
+
+      if (process.env.NODE_ENV === 'development') global._mongoClientConnectionInstance = mongoClientInstance;
+      else this.mongoClientConnectionInstance = mongoClientInstance;
+
+      //
+      // Reset flags
+
+      this.mongoClientConnecting = false;
+      this.mongoClientConnectionRetries = 0;
+
+      //
+    } catch (error) {
+      this.mongoClientConnectionRetries++;
+      if (this.mongoClientConnectionRetries < MAX_CONNECTION_RETRIES) {
+        console.error(`SLAMANAGERDB: Error creating MongoDB Client instance ["${error.message}"]. Retrying (${this.mongoClientConnectionRetries}/${MAX_CONNECTION_RETRIES})...`);
+        await this.reset();
+        await this.connect();
+      } else {
+        console.error('SLAMANAGERDB: Error creating MongoDB Client instance:', error);
+        await this.reset();
+      }
+    }
+
+    //
+  }
+
+  /* * *
+   * RESET CONNECTIONS
+   * Resets all connections and flags
+   */
+
+  async reset() {
+    this.mongoClientConnecting = false;
+    this.mongoClientConnectionInstance = null;
+    global._mongoClientConnectionInstance = null;
+    console.log('SLAMANAGERDB: Reset connection.');
+  }
+
+  async waitForMongoClientConnection() {
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (!this.mongoClientConnecting) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+
+  //
+}
+
+/* * */
+
+module.exports = new SLAMANAGERDB();
