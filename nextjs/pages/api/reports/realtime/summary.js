ac30b8d53d284565c2b2249e1d180d4220fec66b
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-07 17:02:16 +0100

    ESLint whitespace changes

--- nextjs/pages/api/reports/realtime/summary.js
+++ nextjs/pages/api/reports/realtime/summary.js
@@ -13,156 +13,156 @@ export const config = { api: { responseLimit: false } };
 /* * */
 
 export default async function handler(req, res) {
-  //
+	//
 
-  // 1.
-  // Setup variables
+	// 1.
+	// Setup variables
 
-  let sessionData;
+	let sessionData;
 
-  // 2.
-  // Get session data
+	// 2.
+	// Get session data
 
-  try {
-    sessionData = await getSession(req, res);
-  } catch (error) {
-    console.log(error);
-    return await res.status(400).json({ message: error.message || 'Could not get Session data. Are you logged in?' });
-  }
+	try {
+		sessionData = await getSession(req, res);
+	} catch (error) {
+		console.log(error);
+		return await res.status(400).json({ message: error.message || 'Could not get Session data. Are you logged in?' });
+	}
 
-  // 3.
-  // Prepare endpoint
+	// 3.
+	// Prepare endpoint
 
-  try {
-    await prepareApiEndpoint({ request: req, method: 'POST', session: sessionData, permissions: [{ scope: 'reports', action: 'view', fields: [{ key: 'kind', values: ['realtime'] }] }] });
-  } catch (error) {
-    console.log(error);
-    return await res.status(400).json({ message: error.message || 'Could not prepare endpoint.' });
-  }
+	try {
+		await prepareApiEndpoint({ request: req, method: 'POST', session: sessionData, permissions: [{ scope: 'reports', action: 'view', fields: [{ key: 'kind', values: ['realtime'] }] }] });
+	} catch (error) {
+		console.log(error);
+		return await res.status(400).json({ message: error.message || 'Could not prepare endpoint.' });
+	}
 
-  // 4.
-  // Parse request body into JSON
+	// 4.
+	// Parse request body into JSON
 
-  try {
-    req.body = await JSON.parse(req.body);
-  } catch (error) {
-    console.log(error);
-    return await res.status(500).json({ message: 'JSON parse error.' });
-  }
+	try {
+		req.body = await JSON.parse(req.body);
+	} catch (error) {
+		console.log(error);
+		return await res.status(500).json({ message: 'JSON parse error.' });
+	}
 
-  // 5.
-  // Prepare datetime variables
+	// 5.
+	// Prepare datetime variables
 
-  let operationDayStartMilis;
-  let operationDayEndMilis;
+	let operationDayStartMilis;
+	let operationDayEndMilis;
 
-  try {
-    operationDayStartMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').startOf('day').set({ hour: 4, minute: 0, second: 0 }).toMillis();
-    operationDayEndMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').plus({ days: 1 }).startOf('day').set({ hour: 3, minute: 59, second: 59 }).toMillis();
-    //
-    // /* TEST */ operationDayEndMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').startOf('day').set({ hour: 5, minute: 0, second: 0 }).toMillis();
-  } catch (error) {
-    console.log(error);
-    return await res.status(500).json({ message: 'Error converting date boundaries to miliseconds.' });
-  }
+	try {
+		operationDayStartMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').startOf('day').set({ hour: 4, minute: 0, second: 0 }).toMillis();
+		operationDayEndMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').plus({ days: 1 }).startOf('day').set({ hour: 3, minute: 59, second: 59 }).toMillis();
+		//
+		// /* TEST */ operationDayEndMilis = DateTime.fromFormat(req.body.operation_day, 'yyyyMMdd').setZone('Europe/Lisbon').startOf('day').set({ hour: 5, minute: 0, second: 0 }).toMillis();
+	} catch (error) {
+		console.log(error);
+		return await res.status(500).json({ message: 'Error converting date boundaries to miliseconds.' });
+	}
 
-  // 6.
-  // Connect to PCGIDB
+	// 6.
+	// Connect to PCGIDB
 
-  try {
-    await PCGIDB.connect();
-  } catch (error) {
-    console.log(error);
-    return await res.status(500).json({ message: 'Could not connect to PCGIDB.' });
-  }
+	try {
+		await PCGIDB.connect();
+	} catch (error) {
+		console.log(error);
+		return await res.status(500).json({ message: 'Could not connect to PCGIDB.' });
+	}
 
-  // 7.
-  // Prepare aggregation pipeline
+	// 7.
+	// Prepare aggregation pipeline
 
-  const matchClause = {
-    $match: {
-      $and: [
-        {
-          millis: { $gte: operationDayStartMilis, $lte: operationDayEndMilis },
-          $and: [
-            {
-              'content.entity.vehicle.agencyId': req.body.agency_code,
-              $and: [
-                {
-                  'content.entity.vehicle.trip.tripId': { $exists: true, $ne: null },
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  };
+	const matchClause = {
+		$match: {
+			$and: [
+				{
+					millis: { $gte: operationDayStartMilis, $lte: operationDayEndMilis },
+					$and: [
+						{
+							'content.entity.vehicle.agencyId': req.body.agency_code,
+							$and: [
+								{
+									'content.entity.vehicle.trip.tripId': { $exists: true, $ne: null },
+								},
+							],
+						},
+					],
+				},
+			],
+		},
+	};
 
-  const groupClause = {
-    $group: {
-      _id: '$content.entity.vehicle.trip.tripId',
-      trip_id: { $first: '$content.entity.vehicle.trip.tripId' },
-      line_id: { $first: '$content.entity.vehicle.trip.lineId' },
-      route_id: { $first: '$content.entity.vehicle.trip.routeId' },
-      pattern_id: { $first: '$content.entity.vehicle.trip.patternId' },
-      vehicle_id: { $addToSet: '$content.entity.vehicle.vehicle._id' },
-      driver_id: { $addToSet: '$content.entity.vehicle.vehicle.driverId' },
-      num_events: { $sum: 1 },
-      positions: {
-        $push: {
-          tml_event_id: '$_id',
-          operator_event_id: '$content.entity._id',
-          insert_timestamp: '$millis',
-          header_timestamp: '$content.header.timestamp',
-          vehicle_timestamp: '$content.entity.vehicle.timestamp',
-          odometer: '$content.entity.vehicle.position.odometer',
-          lat: '$content.entity.vehicle.position.latitude',
-          lon: '$content.entity.vehicle.position.longitude',
-        },
-      },
-    },
-  };
+	const groupClause = {
+		$group: {
+			_id: '$content.entity.vehicle.trip.tripId',
+			trip_id: { $first: '$content.entity.vehicle.trip.tripId' },
+			line_id: { $first: '$content.entity.vehicle.trip.lineId' },
+			route_id: { $first: '$content.entity.vehicle.trip.routeId' },
+			pattern_id: { $first: '$content.entity.vehicle.trip.patternId' },
+			vehicle_id: { $addToSet: '$content.entity.vehicle.vehicle._id' },
+			driver_id: { $addToSet: '$content.entity.vehicle.vehicle.driverId' },
+			num_events: { $sum: 1 },
+			positions: {
+				$push: {
+					tml_event_id: '$_id',
+					operator_event_id: '$content.entity._id',
+					insert_timestamp: '$millis',
+					header_timestamp: '$content.header.timestamp',
+					vehicle_timestamp: '$content.entity.vehicle.timestamp',
+					odometer: '$content.entity.vehicle.position.odometer',
+					lat: '$content.entity.vehicle.position.latitude',
+					lon: '$content.entity.vehicle.position.longitude',
+				},
+			},
+		},
+	};
 
-  const projectClause = {
-    $project: {
-      _id: 0,
-      trip_id: { $first: '$trip_id' },
-      line_id: { $first: '$line_id' },
-      route_id: { $first: '$route_id' },
-      pattern_id: { $first: '$pattern_id' },
-      vehicle_id: 1,
-      driver_id: 1,
-      num_events: 1,
-      positions: {
-        $map: {
-          input: '$positions',
-          as: 'position',
-          in: {
-            tml_event_id: '$$position.tml_event_id',
-            operator_event_id: { $first: '$$position.operator_event_id' },
-            insert_timestamp: '$$position.insert_timestamp',
-            header_timestamp: '$$position.header_timestamp',
-            vehicle_timestamp: { $first: '$$position.vehicle_timestamp' },
-            odometer: { $first: '$$position.odometer' },
-            lat: { $first: '$$position.lat' },
-            lon: { $first: '$$position.lon' },
-          },
-        },
-      },
-    },
-  };
+	const projectClause = {
+		$project: {
+			_id: 0,
+			trip_id: { $first: '$trip_id' },
+			line_id: { $first: '$line_id' },
+			route_id: { $first: '$route_id' },
+			pattern_id: { $first: '$pattern_id' },
+			vehicle_id: 1,
+			driver_id: 1,
+			num_events: 1,
+			positions: {
+				$map: {
+					input: '$positions',
+					as: 'position',
+					in: {
+						tml_event_id: '$$position.tml_event_id',
+						operator_event_id: { $first: '$$position.operator_event_id' },
+						insert_timestamp: '$$position.insert_timestamp',
+						header_timestamp: '$$position.header_timestamp',
+						vehicle_timestamp: { $first: '$$position.vehicle_timestamp' },
+						odometer: { $first: '$$position.odometer' },
+						lat: { $first: '$$position.lat' },
+						lon: { $first: '$$position.lon' },
+					},
+				},
+			},
+		},
+	};
 
-  // 8.
-  // Perform database search
+	// 8.
+	// Perform database search
 
-  try {
-    console.log('Searching events...');
-    await PCGIDB.VehicleEvents.aggregate([matchClause, groupClause, projectClause], { allowDiskUse: true, maxTimeMS: 90000 }).stream().pipe(JSONStream.stringify()).pipe(res);
-  } catch (error) {
-    console.log(error);
-    return await res.status(500).json({ message: error.message || 'Cannot list VehicleEvents.' });
-  }
+	try {
+		console.log('Searching events...');
+		await PCGIDB.VehicleEvents.aggregate([matchClause, groupClause, projectClause], { allowDiskUse: true, maxTimeMS: 90000 }).stream().pipe(JSONStream.stringify()).pipe(res);
+	} catch (error) {
+		console.log(error);
+		return await res.status(500).json({ message: error.message || 'Cannot list VehicleEvents.' });
+	}
 
-  //
+	//
 }
--- nextjs/schemas/Pattern/model.js
+++ nextjs/schemas/Pattern/model.js
@@ -5,183 +5,183 @@ import mongoose from 'mongoose';
 /* * */
 
 export const PatternSchema = new mongoose.Schema(
-  {
-    code: {
-      type: String,
-      maxlength: 25,
-      unique: true,
-    },
-    parent_line: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Line',
-    },
-    parent_route: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Route',
-    },
-    origin: {
-      type: String,
-      maxlength: 100,
-    },
-    destination: {
-      type: String,
-      maxlength: 100,
-    },
-    headsign: {
-      type: String,
-      maxlength: 100,
-    },
-    presets: {
-      velocity: {
-        type: Number,
-        default: 20,
-      },
-      dwell_time: {
-        type: Number,
-        default: 0,
-      },
-    },
-    shape: {
-      extension: {
-        type: Number,
-      },
-      points: [
-        {
-          shape_pt_lat: {
-            type: Number,
-          },
-          shape_pt_lon: {
-            type: Number,
-          },
-          shape_pt_sequence: {
-            type: Number,
-          },
-          shape_dist_traveled: {
-            type: Number,
-          },
-        },
-      ],
-      geojson: {
-        type: {
-          type: String,
-          maxlength: 100,
-          default: 'Feature',
-        },
-        geometry: {
-          type: {
-            type: String,
-            maxlength: 100,
-            default: 'LineString',
-          },
-          coordinates: [
-            [
-              {
-                type: Number,
-              },
-            ],
-          ],
-        },
-      },
-    },
-    path: [
-      {
-        stop: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Stop',
-        },
-        allow_pickup: {
-          type: Boolean,
-        },
-        allow_drop_off: {
-          type: Boolean,
-        },
-        distance_delta: {
-          type: Number,
-        },
-        default_velocity: {
-          type: Number,
-        },
-        default_travel_time: {
-          type: Number,
-        },
-        default_dwell_time: {
-          type: Number,
-        },
-        zones: [
-          {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Zone',
-          },
-        ],
-      },
-    ],
-    schedules: [
-      {
-        start_time: {
-          type: String,
-          maxlength: 100,
-        },
-        calendars_on: [
-          {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Calendar',
-          },
-        ],
-        calendars_off: [
-          {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Calendar',
-          },
-        ],
-        calendar_desc: {
-          type: String,
-          maxlength: 500,
-        },
-        vehicle_features: {
-          type: {
-            type: String,
-            maxlength: 2,
-            default: '0',
-          },
-          propulsion: {
-            type: String,
-            maxlength: 2,
-            default: '0',
-          },
-          allow_bicycles: {
-            type: Boolean,
-            default: true,
-          },
-          passenger_counting: {
-            type: Boolean,
-          },
-          video_surveillance: {
-            type: Boolean,
-          },
-        },
-        path_overrides: [
-          {
-            sequence_index: {
-              type: Number,
-            },
-            velocity: {
-              type: Number,
-            },
-            travel_time: {
-              type: Number,
-            },
-            dwell_time: {
-              type: Number,
-            },
-          },
-        ],
-      },
-    ],
-    is_locked: {
-      type: Boolean,
-    },
-  },
-  { timestamps: true }
+	{
+		code: {
+			type: String,
+			maxlength: 25,
+			unique: true,
+		},
+		parent_line: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Line',
+		},
+		parent_route: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Route',
+		},
+		origin: {
+			type: String,
+			maxlength: 100,
+		},
+		destination: {
+			type: String,
+			maxlength: 100,
+		},
+		headsign: {
+			type: String,
+			maxlength: 100,
+		},
+		presets: {
+			velocity: {
+				type: Number,
+				default: 20,
+			},
+			dwell_time: {
+				type: Number,
+				default: 0,
+			},
+		},
+		shape: {
+			extension: {
+				type: Number,
+			},
+			points: [
+				{
+					shape_pt_lat: {
+						type: Number,
+					},
+					shape_pt_lon: {
+						type: Number,
+					},
+					shape_pt_sequence: {
+						type: Number,
+					},
+					shape_dist_traveled: {
+						type: Number,
+					},
+				},
+			],
+			geojson: {
+				type: {
+					type: String,
+					maxlength: 100,
+					default: 'Feature',
+				},
+				geometry: {
+					type: {
+						type: String,
+						maxlength: 100,
+						default: 'LineString',
+					},
+					coordinates: [
+						[
+							{
+								type: Number,
+							},
+						],
+					],
+				},
+			},
+		},
+		path: [
+			{
+				stop: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'Stop',
+				},
+				allow_pickup: {
+					type: Boolean,
+				},
+				allow_drop_off: {
+					type: Boolean,
+				},
+				distance_delta: {
+					type: Number,
+				},
+				default_velocity: {
+					type: Number,
+				},
+				default_travel_time: {
+					type: Number,
+				},
+				default_dwell_time: {
+					type: Number,
+				},
+				zones: [
+					{
+						type: mongoose.Schema.Types.ObjectId,
+						ref: 'Zone',
+					},
+				],
+			},
+		],
+		schedules: [
+			{
+				start_time: {
+					type: String,
+					maxlength: 100,
+				},
+				calendars_on: [
+					{
+						type: mongoose.Schema.Types.ObjectId,
+						ref: 'Calendar',
+					},
+				],
+				calendars_off: [
+					{
+						type: mongoose.Schema.Types.ObjectId,
+						ref: 'Calendar',
+					},
+				],
+				calendar_desc: {
+					type: String,
+					maxlength: 500,
+				},
+				vehicle_features: {
+					type: {
+						type: String,
+						maxlength: 2,
+						default: '0',
+					},
+					propulsion: {
+						type: String,
+						maxlength: 2,
+						default: '0',
+					},
+					allow_bicycles: {
+						type: Boolean,
+						default: true,
+					},
+					passenger_counting: {
+						type: Boolean,
+					},
+					video_surveillance: {
+						type: Boolean,
+					},
+				},
+				path_overrides: [
+					{
+						sequence_index: {
+							type: Number,
+						},
+						velocity: {
+							type: Number,
+						},
+						travel_time: {
+							type: Number,
+						},
+						dwell_time: {
+							type: Number,
+						},
+					},
+				],
+			},
+		],
+		is_locked: {
+			type: Boolean,
+		},
+	},
+	{ timestamps: true },
 );
 
 /* * */
