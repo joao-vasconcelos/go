85af1f4891564258db8fd3a2bfd6903e201aef9d
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-05-07 21:25:27 +0100

    Cleanup

--- slamanager-monitor/services/PCGIDB.js
+++ slamanager-monitor/services/PCGIDB.js
@@ -200,7 +200,7 @@ class PCGIDB {
 			//
 			// Create the SHH Tunnel connection
 
-			const [server, client] = await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+			const [server, _] = await createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
 
 			console.log(`PCGIDB: Created SSH Tunnel instance on host port ${server.address().port}`);
 
