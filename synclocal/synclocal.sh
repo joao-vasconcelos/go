33a3a74f6c3b0a97f6cf2beacea444e509b6e90e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-06-06 02:39:30 +0100

    Update synclocal.sh

--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -2,10 +2,10 @@
 
 # Restore the database from backup
 echo "Starting restore..."
-mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20240603101105"
+mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20240606012326"
 echo "Restore complete!"
 
 # Restore the database from backup
-# echo "Starting restore..."
-# mongorestore --uri="mongodb://slamanagerdbuser:slamanagerdbpassword@slamanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerdb-20240523074834"
-# echo "Restore complete!"
+echo "Starting restore..."
+mongorestore --uri="mongodb://slamanagerdbuser:slamanagerdbpassword@slamanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerdb-20240606012337"
+echo "Restore complete!"
