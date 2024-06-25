623f08f44490ecbe333dbe1122d756730b35beff
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-06-26 00:01:26 +0100

    Update synclocal.sh

--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -2,15 +2,15 @@
 
 # Restore the database from backup
 echo "Starting 'offermanagerdb' restore..."
-mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20240624212407"
+mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20240625205058"
 echo "Restore 'offermanagerdb' complete!"
 
 # Restore the database from backup
 echo "Starting 'slamanagerdb' restore..."
-mongorestore --uri="mongodb://slamanagerdbuser:slamanagerdbpassword@slamanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerdb-20240624212417"
+mongorestore --uri="mongodb://slamanagerdbuser:slamanagerdbpassword@slamanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerdb-20240625205106"
 echo "Restore 'slamanagerdb' complete!"
 
 # Restore the database from backup
 echo "Starting 'slamanagerqueuedb' restore..."
-mongorestore --uri="mongodb://slamanagerqueuedbuser:slamanagerqueuedbpassword@slamanagerqueuedb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerqueuedb-20240624213828"
+mongorestore --uri="mongodb://slamanagerqueuedbuser:slamanagerqueuedbpassword@slamanagerqueuedb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-slamanagerqueuedb-20240625205108"
 echo "Restore 'slamanagerqueuedb' complete!"
