0eb26d769755d6e27ab5edceccc12916384d186c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-01 12:20:16 +0100

    Use prepare endpoint

--- nextjs/pages/api/typologies/[_id]/index.js
+++ nextjs/pages/api/typologies/[_id]/index.js
@@ -1,9 +1,8 @@
 /* * */
 
 import getSession from '@/authentication/getSession';
-import isAllowed from '@/authentication/isAllowed';
 import { TypologyModel } from '@/schemas/Typology/model';
-import mongodb from '@/services/OFFERMANAGERDB';
+import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 
 /* * */
 
@@ -16,37 +15,28 @@ export default async function handler(req, res) {
 	let sessionData;
 
 	// 2.
-	// Refuse request if not GET
-
-	if (req.method != 'GET') {
-		await res.setHeader('Allow', ['GET']);
-		return await res.status(405).json({ message: `Method ${req.method} Not Allowed.` });
-	}
-
-	// 3.
-	// Check for correct Authentication and valid Permissions
+	// Get session data
 
 	try {
 		sessionData = await getSession(req, res);
-		isAllowed(sessionData, [{ action: 'view', scope: 'typologies' }]);
 	}
 	catch (error) {
 		console.log(error);
-		return await res.status(401).json({ message: error.message || 'Could not verify Authentication.' });
+		return await res.status(400).json({ message: error.message || 'Could not get Session data. Are you logged in?' });
+	}
+
+	// 3.
+	// Prepare endpoint
+
+	try {
+		await prepareApiEndpoint({ method: 'GET', permissions: [{ action: 'view', scope: 'typologies' }], request: req, session: sessionData });
+	}
+	catch (error) {
+		console.log(error);
+		return await res.status(400).json({ message: error.message || 'Could not prepare endpoint.' });
 	}
 
 	// 4.
-	// Connect to MongoDB
-
-	try {
-		await mongodb.connect();
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(500).json({ message: 'MongoDB connection error.' });
-	}
-
-	// 5.
 	// Fetch the requested document
 
 	try {
