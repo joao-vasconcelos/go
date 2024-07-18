46640efaa95f8013491cdb4949c2ec9650cad06a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-18 19:16:28 +0100

    Test create table

--- slamanager-buffer/services/SLAMANAGERDBBRIDGE.js
+++ slamanager-buffer/services/SLAMANAGERDBBRIDGE.js
@@ -15,6 +15,15 @@ const client = new pg.Client({
 
 async function connect() {
 	await client.connect();
+	await client.query(`CREATE TABLE municipalities (
+			municipality_prefix VARCHAR(2),
+			municipality_id VARCHAR(4),
+			municipality_name VARCHAR(255),
+			district_id VARCHAR(255),
+			district_name VARCHAR(255),
+			region_id VARCHAR(255),
+			region_name VARCHAR(255)
+		);`);
 	LOGGER.success('Connected to NETWORKDB');
 }
 
