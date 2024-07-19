e30e03a18fa1068e496322b2f401c468a1694977
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-19 18:56:16 +0100

    Update NPM (temp)

--- slamanager-bridge/services/SLAMANAGERBRIDGEDB.js
+++ slamanager-bridge/services/SLAMANAGERBRIDGEDB.js
@@ -7,20 +7,26 @@ import pg from 'pg';
 
 const client = new pg.Client({
 	connectionTimeoutMillis: 10000,
-	database: 'networkdbuser',
-	host: 'slamanagerdb-bridge',
-	password: 'networkdbpassword',
-	user: 'networkdbuser',
+	database: process.env.SLAMANAGERBRIDGEDB_DB,
+	host: process.env.SLAMANAGERBRIDGEDB_HOST,
+	password: process.env.SLAMANAGERBRIDGEDB_PASSWORD,
+	user: process.env.SLAMANAGERBRIDGEDB_USER,
 });
 
 async function connect() {
+	console.log('------');
+	console.log(process.env.SLAMANAGERBRIDGEDB_DB);
+	console.log(process.env.SLAMANAGERBRIDGEDB_HOST);
+	console.log(process.env.SLAMANAGERBRIDGEDB_USER);
+	console.log(process.env.SLAMANAGERBRIDGEDB_PASSWORD);
+	console.log('------');
 	await client.connect();
-	LOGGER.success('Connected to NETWORKDB');
+	LOGGER.success('Connected to SLAMANAGERBRIDGEDB');
 }
 
 async function disconnect() {
 	await client.end();
-	LOGGER.success('Disconnected from NETWORKDB');
+	LOGGER.success('Disconnected from SLAMANAGERBRIDGEDB');
 }
 
 /* * */
