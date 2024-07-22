7de71d6c064505f4ed608c8bc16f25877a1d24bf
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-07-22 10:58:50 +0100

    First try

--- slamanager-bridge/start.ts
+++ slamanager-bridge/start.ts
@@ -8,80 +8,100 @@ import TIMETRACKER from '@helperkits/timer';
 
 /* * */
 
+async function createTableFromExample(tripAnalysisParsed) {
+	const keys = Object.keys(tripAnalysisParsed);
+	const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS trip_analysis (
+            ${keys.map(key => `${key} TEXT`).join(',')}
+        );
+    `;
+	await SLAMANAGERBRIDGEDB.client.query(createTableQuery);
+}
+
+/* * */
+
+async function insertDataInBatches(data, batchSize = 1000) {
+	const keys = Object.keys(data[0]);
+	const valuesPlaceholder = keys.map((_, i) => `$${i + 1}`).join(',');
+
+	const insertQuery = `
+        INSERT INTO trip_analysis (${keys.join(',')})
+        VALUES (${valuesPlaceholder})
+        ON CONFLICT DO NOTHING;
+    `;
+
+	for (let i = 0; i < data.length; i += batchSize) {
+		const batch = data.slice(i, i + batchSize);
+		const values = batch.map(item => keys.map(key => item[key]));
+		await SLAMANAGERBRIDGEDB.client.query(insertQuery, values.flat());
+	}
+}
+
+/* * */
+
+function parseTripAnalysis(tripAnalysis) {
+	const parsed = {
+		agency_id: tripAnalysis.agency_id,
+		archive_id: tripAnalysis.archive_id,
+		code: tripAnalysis.code,
+		line_id: tripAnalysis.line_id,
+		operational_day: tripAnalysis.operational_day,
+		pattern_id: tripAnalysis.pattern_id,
+		route_id: tripAnalysis.route_id,
+		scheduled_start_time: tripAnalysis.scheduled_start_time,
+		service_id: tripAnalysis.service_id,
+		trip_id: tripAnalysis.trip_id,
+		user_notes: tripAnalysis.user_notes,
+	};
+
+	tripAnalysis.analysis.forEach((item) => {
+		parsed[`${item.code}_status`] = item.status;
+		parsed[`${item.code}_grade`] = item.grade;
+		parsed[`${item.code}_reason`] = item.reason;
+		parsed[`${item.code}_message`] = item.message;
+		parsed[`${item.code}_unit`] = item.unit;
+		parsed[`${item.code}_value`] = item.value;
+	});
+
+	return parsed;
+}
+
+/* * */
+
 export default async () => {
 	try {
-		//
-
 		LOGGER.init();
-
 		const globalTimer = new TIMETRACKER();
 
-		//
-		// Connect to databases
-
 		LOGGER.info('Connecting to databases...');
-
-		await SLAMANAGERDB.connect();
-		await SLAMANAGERBUFFERDB.connect();
-		await SLAMANAGERBRIDGEDB.connect();
+		await Promise.all([
+			SLAMANAGERDB.connect(),
+			SLAMANAGERBUFFERDB.connect(),
+			SLAMANAGERBRIDGEDB.connect(),
+		]);
 
 		LOGGER.divider();
-
-		//
-		// Create SLAMANAGERBRIDGEDB tables
-
 		LOGGER.info('Creating TripAnalysis tables...');
 
 		const exampleTripAnalysis = await SLAMANAGERDB.TripAnalysis.findOne();
+		if (!exampleTripAnalysis) {
+			throw new Error('No example trip analysis found.');
+		}
 
-		const tripAnalysisParsed = {
-			agency_id: exampleTripAnalysis.agency_id,
-			archive_id: exampleTripAnalysis.archive_id,
-			code: exampleTripAnalysis.code,
-			line_id: exampleTripAnalysis.line_id,
-			operational_day: exampleTripAnalysis.operational_day,
-			pattern_id: exampleTripAnalysis.pattern_id,
-			route_id: exampleTripAnalysis.route_id,
-			scheduled_start_time: exampleTripAnalysis.scheduled_start_time,
-			service_id: exampleTripAnalysis.service_id,
-			trip_id: exampleTripAnalysis.trip_id,
-			user_notes: exampleTripAnalysis.user_notes,
-		};
+		const tripAnalysisParsed = parseTripAnalysis(exampleTripAnalysis);
+		await createTableFromExample(tripAnalysisParsed);
 
-		exampleTripAnalysis.analysis.forEach((item) => {
-			tripAnalysisParsed[`${item.code}_status`] = item.status;
-			tripAnalysisParsed[`${item.code}_grade`] = item.grade;
-			tripAnalysisParsed[`${item.code}_reason`] = item.reason;
-			tripAnalysisParsed[`${item.code}_message`] = item.message;
-			tripAnalysisParsed[`${item.code}_unit`] = item.unit;
-			tripAnalysisParsed[`${item.code}_value`] = item.value;
-		});
-
-		// Get keys from object
-
-		const keys = Object.keys(tripAnalysisParsed);
-
-		// Create table
-
-		await SLAMANAGERBRIDGEDB.client.query(`
-			CREATE TABLE trip_analysis (
-				${keys.map(key => `${key} VARCHAR(255)`).join(',')}
-			);
-		`);
-
-		//
+		const allTripAnalyses = await SLAMANAGERDB.TripAnalysis.find();
+		for await (const tripAnalysis of allTripAnalyses) {
+			const parsedTripAnalysis = parseTripAnalysis(tripAnalysis);
+			await insertDataInBatches([parsedTripAnalysis]);
+		}
 
 		LOGGER.terminate(`Run took ${globalTimer.get()}.`);
-
-		//
 	}
 	catch (err) {
-		console.log('An error occurred. Halting execution.', err);
-		console.log('Retrying in 10 seconds...');
-		setTimeout(() => {
-			process.exit(0); // End process
-		}, 10000); // after 10 seconds
+		LOGGER.error('An error occurred. Halting execution.', err);
+		LOGGER.info('Retrying in 10 seconds...');
+		setTimeout(() => process.exit(1), 10000);
 	}
-
-	//
 };
