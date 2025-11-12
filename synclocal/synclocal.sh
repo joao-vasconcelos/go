3f2e0a6691927e42d2062ed29f81a5557cba470c
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-11-12 12:00:19 +0000

    Update synclocal

--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -3,5 +3,5 @@
 # # #
 
 echo "Starting 'offermanagerdb' restore..."
-mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20251029131424"
+mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20251112065638"
 echo "Restore 'offermanagerdb' complete!"
