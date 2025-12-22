999a46ada16842574afcde87536275963408fa4f
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-22 18:50:52 +0000

    Fix

--- nextjs/pages/api/stops/public.ts
+++ nextjs/pages/api/stops/public.ts
@@ -11,8 +11,8 @@ export default async function handler(req, res) {
 		const allDocuments = await StopModel.find({});
 		const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
 		const sortedDocuments = allDocuments
-			.sort((a, b) => collator.compare(a.code, b.code))
-			.map(doc => ({ ...doc, latitude: Number(Number(doc.latitude).toFixed(6)), longitude: Number(Number(doc.longitude).toFixed(6)) }));
+			.map(doc => ({ ...doc, latitude: Number(Number(doc.latitude).toFixed(6)), longitude: Number(Number(doc.longitude).toFixed(6)) }))
+			.sort((a, b) => collator.compare(a.code, b.code));
 		return await res.status(200).send(sortedDocuments);
 	}
 	catch (error) {
