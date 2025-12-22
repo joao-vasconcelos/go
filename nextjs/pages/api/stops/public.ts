73b123dcbe042b5a67018a9384d18b4739be0006
João de Vasconcelos <johnyvasconcelos@icloud.com>
2025-12-22 18:16:20 +0000

    Clamp coordinates to 6 decimal places

--- nextjs/pages/api/stops/public.ts
+++ nextjs/pages/api/stops/public.ts
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
 	try {
 		const allDocuments = await StopModel.find({});
 		const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' });
-		const sortedDocuments = allDocuments.sort((a, b) => collator.compare(a.code, b.code));
+		const sortedDocuments = allDocuments
+			.sort((a, b) => collator.compare(a.code, b.code))
+			.map(doc => ({ ...doc, latitude: Number(Number(doc.latitude).toFixed(6)), longitude: Number(Number(doc.longitude).toFixed(6)) }));
 		return await res.status(200).send(sortedDocuments);
 	}
 	catch (error) {
