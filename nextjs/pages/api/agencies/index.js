c579516b971e7c06ff2d3393257458a547162f09
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-02-04 17:52:48 +0000

    Remove auth on list endpoints

--- nextjs/pages/api/agencies/index.js
+++ nextjs/pages/api/agencies/index.js
@@ -1,53 +1,13 @@
 /* * */
 
-import getSession from '@/authentication/getSession';
 import { AgencyModel } from '@/schemas/Agency/model';
-import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 
 /* * */
 
 export default async function handler(req, res) {
 	//
 
-	// 1.
-	// Setup variables
-
-	let sessionData;
-
-	// 2.
-	// Get session data
-
-	try {
-		sessionData = await getSession(req, res);
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(400).json({ message: error.message || 'Could not get Session data. Are you logged in?' });
-	}
-
-	// 3.
-	// Prepare endpoint
-
-	try {
-		await prepareApiEndpoint({ method: 'GET', permissions: [{ action: 'view', scope: 'agencies' }], request: req, session: sessionData });
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(400).json({ message: error.message || 'Could not prepare endpoint.' });
-	}
-
-	// 4.
-	// Ensure latest schema modifications are applied in the database
-
-	try {
-		await AgencyModel.syncIndexes();
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(500).json({ message: 'Cannot sync indexes.' });
-	}
-
-	// 5.
+	//
 	// List all documents
 
 	try {
--- nextjs/pages/api/zones/index.js
+++ nextjs/pages/api/zones/index.js
@@ -1,8 +1,6 @@
 /* * */
 
-import getSession from '@/authentication/getSession';
 import { ZoneModel } from '@/schemas/Zone/model';
-import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 
 /* * */
 
@@ -13,45 +11,7 @@ export const config = { api: { responseLimit: false } };
 export default async function handler(req, res) {
 	//
 
-	// 1.
-	// Setup variables
-
-	let sessionData;
-
-	// 2.
-	// Get session data
-
-	try {
-		sessionData = await getSession(req, res);
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(400).json({ message: error.message || 'Could not get Session data. Are you logged in?' });
-	}
-
-	// 3.
-	// Prepare endpoint
-
-	try {
-		await prepareApiEndpoint({ method: 'GET', permissions: [{ action: 'view', scope: 'zones' }], request: req, session: sessionData });
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(400).json({ message: error.message || 'Could not prepare endpoint.' });
-	}
-
-	// 4.
-	// Ensure latest schema modifications are applied in the database
-
-	try {
-		await ZoneModel.syncIndexes();
-	}
-	catch (error) {
-		console.log(error);
-		return await res.status(500).json({ message: 'Cannot sync indexes.' });
-	}
-
-	// 5.
+	//
 	// List all documents
 
 	try {
