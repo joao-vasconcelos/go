d9aab0bbccf12d1a3b9a34daf6688736eff781d6
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-12 19:42:21 +0000

    Cleanup

--- nextjs/pages/api/configs/refactors/remove-special-calendars.js
+++ nextjs/pages/api/configs/refactors/remove-special-calendars.js
@@ -10,7 +10,7 @@ import prepareApiEndpoint from '@/services/prepareApiEndpoint';
 export default async function handler(req, res) {
 	//
 
-	throw new Error('Feature is disabled.');
+	// throw new Error('Feature is disabled.');
 
 	// 1.
 	// Setup variables
@@ -52,6 +52,9 @@ export default async function handler(req, res) {
 					'ESP_NATAL_DIA',
 					'ESP_ANONOVO_VESP',
 					'ESP_ANONOVO_DIA',
+					'ESP_SANTOS_VESP',
+					'ESP_SANTOS_DIA',
+					'ESP_CARNAVAL_DIA',
 				],
 			},
 		}, '_id code');
@@ -66,7 +69,7 @@ export default async function handler(req, res) {
 		for (const patternCode of allPatternCodesWithSpecialCalendars) {
 			//
 
-			if (patternCode.code.startsWith('1')) continue;
+			// if (patternCode.code.startsWith('1')) continue;
 			// if (patternCode.code.startsWith('2')) continue;
 			// if (patternCode.code.startsWith('3')) continue;
 			// if (patternCode.code.startsWith('4')) continue;
--- synclocal/synclocal.sh
+++ synclocal/synclocal.sh
@@ -3,5 +3,5 @@
 # # #
 
 echo "Starting 'offermanagerdb' restore..."
-mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20251112065638"
+mongorestore --uri="mongodb://offermanagerdbuser:offermanagerdbpassword@offermanagerdb/production?authSource=admin" --drop --preserveUUID --gzip --archive="go-backup-offermanagerdb-20260302090643"
 echo "Restore 'offermanagerdb' complete!"
