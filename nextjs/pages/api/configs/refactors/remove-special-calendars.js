282690d82bf76954a740258cbb6143fe9253aca0
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-04-02 10:59:00 +0100

    Fix

--- nextjs/pages/api/configs/refactors/remove-special-calendars.js
+++ nextjs/pages/api/configs/refactors/remove-special-calendars.js
@@ -69,10 +69,10 @@ export default async function handler(req, res) {
 		for (const patternCode of allPatternCodesWithSpecialCalendars) {
 			//
 
-			if (patternCode.code.startsWith('1')) continue;
+			// if (patternCode.code.startsWith('1')) continue;
 			if (patternCode.code.startsWith('2')) continue;
 			if (patternCode.code.startsWith('3')) continue;
-			// if (patternCode.code.startsWith('4')) continue;
+			if (patternCode.code.startsWith('4')) continue;
 
 			//
 
